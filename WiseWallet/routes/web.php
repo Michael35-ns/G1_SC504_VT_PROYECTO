e4246f5b6b1bde60b9a90e83d66a5172bd35f214
FabianMataT <145735103+FabianMataT@users.noreply.github.com>
2024-07-12 20:13:49 -0600

    Conexion Exitosa DB
    Se termino de establecer la conexion y ya se pueden hacer selects desde el proyecto

--- WiseWallet/routes/web.php
+++ WiseWallet/routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    $roles = DB::table('FIDE_ROL_TB')->get();
+    return view('welcome');
+});
+
+/*
+Route::get('/', function () {
+    $roles = DB::table('FIDE_ROL_TB')
+                ->select(
+                    'ID_ROL as id_rol',
+                    'DESCRIPCION as descripcion',
+                    'NOMBRE_ROL as nombre_rol',
+                    'USUARIO_REG as usuario_reg',
+                    'FECHA_ACCION as fecha_accion',
+                    'ACCION as accion'
+                )
+                ->get();
     return view('welcome', ['roles' => $roles]);
 });
+*/
