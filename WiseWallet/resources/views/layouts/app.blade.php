96c28168ee61c9510635862cd7dfd7439ad886b0
FabianMataT <145735103+FabianMataT@users.noreply.github.com>
2024-07-20 22:58:54 -0600

    Interfaz lista.
    Tambien la del registro

--- WiseWallet/resources/views/layouts/app.blade.php
+++ WiseWallet/resources/views/layouts/app.blade.php
@@ -9,55 +9,90 @@
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-dTZpGPg9lb+4m6X35iYJPL3wWyWZ8HRl3rJgtT5V3Id+ZjI5/7MXaGQCUFZ0O2GjVwBdltFl+slylbE7RYyzlA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        integrity="sha512-dTZpGPg9lb+4m6X35iYJPL3wWyWZ8HRl3rJgtT5V3Id+ZjI5/7MXaGQCUFZ0O2GjVwBdltFl+slylbE7RYyzlA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body class="hold-transition sidebar-mini bg-gray-100 h-screen flex flex-col">
-    <nav class="h-auto text-white p-2 bg-gray-900 flex gap-2 justify-between items-center">
-        <div class="w-max bg group flex items-center space-x-4 text-white bg-gray-900">
-            <img src="https://cdn-icons-png.flaticon.com/512/3058/3058953.png" class="w-12" alt="Logo">
-            <span class="-mr-1 font-medium">WiseWallet</span>
-        </div>
-        <div>
-            <a href="#" class="usuario_img">
-                <img src="https://cdn-icons-png.flaticon.com/512/1057/1057231.png" class="w-11" alt="Usuario">
-            </a>
-        </div>
-    </nav>
+    @auth
+        <nav class="h-auto text-white p-2 bg-gray-900 flex gap-2 justify-between items-center">
+            <div class="w-max bg group flex items-center space-x-4 text-white bg-gray-900">
+                <img src="https://cdn-icons-png.flaticon.com/512/3058/3058953.png" class="w-12" alt="Logo">
+                <span class="-mr-1 font-medium">WiseWallet</span>
+            </div>
+            <div>
+                <a class="fond-bold text-white text-sm" href="#">
+                    Hola:
+                    <span class="font-normal">
+                        {{ auth()->user()->username }}
+                    </span>
+                </a>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="fond-bold uppercase text-white text-sm">Cerrar Sesión</button>
+                </form>
+            </div>
+        </nav>
+    @endauth
+    @guest
+        <nav class="h-auto text-white p-2 bg-gray-900 flex gap-2 justify-between items-center">
+            <div class="w-max bg group flex items-center space-x-4 text-white bg-gray-900">
+                <img src="https://cdn-icons-png.flaticon.com/512/3058/3058953.png" class="w-12" alt="Logo">
+                <span class="-mr-1 font-medium">WiseWallet</span>
+            </div>
+            <div>
+                <a href="{{ route('login') }}" class="usuario_img" >
+                    <img src="https://cdn-icons-png.flaticon.com/512/1057/1057231.png" class="w-11" alt="Usuario">
+                </a>
+            </div>
+        </nav>
+    @endguest
 
     <div class="flex flex-1 overflow-y-auto">
         <aside class="bg-gray-900 custom-aside h-full z-50">
-            <div class="sidebar w-[4.15rem] overflow-hidden border-r border-gray-700 hover:w-60 hover:bg-gray-900 hover:shadow-lg transition-all duration-300 h-full">
+            <div
+                class="sidebar w-[4.15rem] overflow-hidden border-r border-gray-700 hover:w-60 hover:bg-gray-900 hover:shadow-lg transition-all duration-300 h-full">
                 <div class="flex h-full flex-col justify-between pt-2 pb-6">
                     <div>
                         <ul class="mt-6 space-y-2 tracking-wide">
                             <li class="min-w-max">
-                                <a href="/" aria-label="home" class="bg group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/11668/11668367.png" class="w-9" alt="home">
+                                <a href="/" aria-label="home"
+                                    class="bg group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/11668/11668367.png" class="w-9"
+                                        alt="home">
                                     <span class="-mr-1 font-medium">Página principal</span>
                                 </a>
                             </li>
                             <li class="min-w-max">
-                                <a href="{{ route('crearIngreso') }}" class="bg group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
-                                    <img src="https://cdn-icons-png.flaticon.com/256/1455/1455976.png" class="w-9" alt="Transacciones">
+                                <a href="{{ route('crearIngreso') }}"
+                                    class="bg group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
+                                    <img src="https://cdn-icons-png.flaticon.com/256/1455/1455976.png" class="w-9"
+                                        alt="Transacciones">
                                     <span class="group-hover:text-gray-300">Transacciones</span>
                                 </a>
                             </li>
                             <li class="min-w-max">
-                                <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/2990/2990468.png" class="w-9" alt="Reseñas">
+                                <a href="#"
+                                    class="group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/2990/2990468.png" class="w-9"
+                                        alt="Reseñas">
                                     <span class="group-hover:text-gray-300">Reseñas</span>
                                 </a>
                             </li>
                             <li class="min-w-max">
-                                <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/5917/5917200.png" class="w-9" alt="ObjetivosEconomicos">
+                                <a href="#"
+                                    class="group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/5917/5917200.png" class="w-9"
+                                        alt="ObjetivosEconomicos">
                                     <span class="group-hover:text-gray-300">Objetivos Económicos</span>
                                 </a>
                             </li>
                             <li class="min-w-max">
-                                <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/4703/4703578.png" class="w-9" alt="Presupuestos">
+                                <a href="#"
+                                    class="group flex items-center space-x-4 rounded-md px-4 py-3 text-white hover:bg-gray-800 transition-all duration-300">
+                                    <img src="https://cdn-icons-png.flaticon.com/512/4703/4703578.png" class="w-9"
+                                        alt="Presupuestos">
                                     <span class="group-hover:text-gray-300">Presupuestos</span>
                                 </a>
                             </li>
@@ -67,7 +102,8 @@
             </div>
         </aside>
         <main class="w-full p-5 min-h-10 overflow-y-auto">
-            <div class="flex flex-col p-4 md:gap-8 gap-4 h-full border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+            <div
+                class="flex flex-col p-4 md:gap-8 gap-4 h-full ">
                 <h1 class="font-black text-gray-900 text-center text-5xl">
                     @yield('titulo')
                 </h1>
--- WiseWallet/routes/web.php
+++ WiseWallet/routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
-use App\Http\Controllers\GraficoController;
-use App\Http\Controllers\IngresoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\IngresoController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login',[LoginController::class, 'index'])->name('login');
+Route::post('/login',[LoginController::class,'store']);
+Route::post('/logout',[LogoutController::class, 'store'])->name('logout');
 
 Route::get('/crearIngreso', [IngresoController::class, 'index'])->name('crearIngreso');
 Route::get('/crearGasto', [IngresoController::class, 'index'])->name('crearGasto');
