59449dbd4d944f959f6fa973069d00eb4da17087
JonathanGG <jimenezzjti99@gmail.com>
2024-07-26 16:49:49 -0600

    nuevo comienzo set-up

--- WiseWallet/resources/views/components/ui/input.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['labelName' => '', 'type' => 'text'])
-
-
-<div class="flex items-stretch max-h-16">
-    <label for="presupuesto-search" class="hidden"> {{ $labelName }} </label>
-    <input type="{{ $type }}" name="presupuesto-search" id="presupuesto-search" placeholder="{{ $labelName }}"
-        {{ $attributes->class(['border-solid border-2 border-gray-400 rounded-sm py-3 pl-8 pr-12 font-outfit font-normal']) }} />
-</div>
